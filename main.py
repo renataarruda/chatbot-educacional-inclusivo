3c3f203b4bc4a9edbc8426dddd1f3a3e6ee0f5d8
Renata Arruda <renata.arruda85@gmail.com>
2026-03-11 14:48:19 -0300

    feat: criando interface do chatbot no terminal

--- main.py
+++ main.py
@@ -40,31 +40,105 @@ Seu papel é oferecer orientação inicial, esclarecer dúvidas e ajudar o usuá
 #     "response_mime_type": "text/plain"
 # }
 
-llm = genai.GenerativeModel(
+def criar_modelo():
+    return genai.GenerativeModel(
     model_name=MODELO_ESCOLHIDO,
     system_instruction=prompt_sistema,
     # generation_config=configuracao_modelo
 )
 
-def gerar_resposta(pergunta):
+def gerar_resposta(llm, pergunta):
     if MODO_DEBUG:
-        print("Modo debug ativado")
+        print("[DEBUG] Usando resposta simulada")
         if "superdotado" in pergunta.lower():
-            return "Mock: Alunos superdotados se beneficiam de atividades abertas e investigativas."
+            return "Mock: Alunos superdotados se beneficiam de atividades abertas e investigativas. Gostaria de fazer outra pergunta?"
 
         if "inclusão" in pergunta.lower():
-            return "Mock: A inclusão escolar envolve adaptar práticas pedagógicas para atender diferentes necessidades."
+            return "Mock: A inclusão escolar envolve adaptar práticas pedagógicas para atender diferentes necessidades. Gostaria de fazer outra pergunta?"
 
         return "Mock: resposta genérica do assistente educacional."
     
     resposta = llm.generate_content(pergunta)
     return resposta.text
 
-print("Enviando pergunta para o modelo...")
+def iniciar_chat(llm, mensagem_inicial):
 
-pergunta = "Como adaptar atividades para um aluno superdotado?"
+    print(mensagem_inicial)
 
-resposta = gerar_resposta(pergunta)
+    while True:
 
-print("Resposta recebida!")
-print(f"A resposta gerada para a pergunta é: {resposta}")
+        pergunta = input("\nVocê: ")
+
+        if pergunta.lower() == "sair":
+            finalizar_chatbot()
+            break
+
+        resposta = gerar_resposta(llm, pergunta)
+
+        print("-" * 50)
+        print("Assistente:", resposta)
+        print("-" * 50)
+
+def finalizar_chatbot():
+    print("Encerrando o chatbot. Até mais!")
+    # exit()
+
+def opcao_invalida():
+    print('Opção inválida\n')
+
+def retornar_menu_principal():
+    input('\nDigite enter para voltar para o menu principal ')
+    main()
+
+def exibir_apresentacao_chatbot():
+    print("""
+Olá! Seja muito bem-vindo(a)! 
+Sou o seu assistente educacional especializado em educação inclusiva, com foco em Altas Habilidades e Superdotação.
+Meu papel é oferecer orientações, esclarecer dúvidas e ajudar você a entender melhor como apoiar a inclusão de crianças e jovens com esse perfil. 
+        """)
+
+def exibir_opcoes():
+    llm = criar_modelo()
+    opcao_escolhida = input("""
+Para que eu possa oferecer a melhor orientação, informe o seu perfil:
+1 - Mãe, pai ou responsável familiar
+2 - Professor(a)
+3 - Gestor(a) escolar
+4 - Outro 
+5 - Sair
+
+Digite aqui: """)
+
+    if opcao_escolhida == "1":
+        iniciar_chat(llm, """
+Que alegria ter você aqui!
+Fique à vontade para contar sua dúvida.
+""")
+    elif opcao_escolhida == "2":
+        iniciar_chat(llm, """
+Olá professor(a)!
+Qual é sua dúvida pedagógica hoje?
+""")
+    elif opcao_escolhida == "3":
+        iniciar_chat(llm,"""
+Olá gestor(a)!
+Como posso ajudar na inclusão escolar?
+""")
+    elif opcao_escolhida == "4":
+        iniciar_chat(llm,"""
+Olá! Qual é sua dúvida sobre inclusão ou altas habilidades?
+""")
+    elif opcao_escolhida == "5":
+        finalizar_chatbot()
+        return
+    else:
+        opcao_invalida()
+        retornar_menu_principal()
+        
+def main():
+    os.system('cls')
+    exibir_apresentacao_chatbot()
+    exibir_opcoes()
+
+if __name__ == "__main__":
+    main()
