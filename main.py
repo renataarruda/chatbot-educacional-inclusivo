b12ef0e54285ee3b581f03b9c72dd2c9a8a75d25
Renata Arruda <renata.arruda85@gmail.com>
2026-03-11 12:16:41 -0300

    feat: criando mock para simular API da IA

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ load_dotenv()
 CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=CHAVE_API_GOOGLE)
 MODELO_ESCOLHIDO = "gemini-3-flash-preview"
+MODO_DEBUG = True
 
 prompt_sistema = """Você é um assistente educacional especializado em educação inclusiva e superdotação.
 
@@ -45,12 +46,25 @@ llm = genai.GenerativeModel(
     # generation_config=configuracao_modelo
 )
 
+def gerar_resposta(pergunta):
+    if MODO_DEBUG:
+        print("Modo debug ativado")
+        if "superdotado" in pergunta.lower():
+            return "Mock: Alunos superdotados se beneficiam de atividades abertas e investigativas."
+
+        if "inclusão" in pergunta.lower():
+            return "Mock: A inclusão escolar envolve adaptar práticas pedagógicas para atender diferentes necessidades."
+
+        return "Mock: resposta genérica do assistente educacional."
+    
+    resposta = llm.generate_content(pergunta)
+    return resposta.text
+
 print("Enviando pergunta para o modelo...")
 
 pergunta = "Como adaptar atividades para um aluno superdotado?"
 
+resposta = gerar_resposta(pergunta)
+
 print("Resposta recebida!")
-
-resposta = llm.generate_content(pergunta)
-
-print(f"A resposta gerada para a pergunta é: {resposta.text}")
+print(f"A resposta gerada para a pergunta é: {resposta}")
